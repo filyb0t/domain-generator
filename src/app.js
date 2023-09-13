5b1349278bb8c867717918af5ce89f09c2e04913
Felix Uriel Lara Castro <138715170+filyb0t@users.noreply.github.com>
2023-09-13 00:30:25 +0000

    domaingenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let articles = ["the", "an", "some", "another", "any"];
+  let adjectives = ["funny", "great", "stunning", "awesome", "thrilling"];
+  let nouns = ["website", "blog", "webapp", "spot", "hub"];
+  let tlds = [
+    ".com",
+    ".space",
+    ".org",
+    ".cln",
+    ".rocks",
+    ".cat",
+    ".dog",
+    ".blockbuster"
+  ];
+
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          console.log(`${article}${adjective}${noun}${tld}`);
+        }
+      }
+    }
+  }
 };
